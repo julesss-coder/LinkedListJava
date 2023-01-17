3da6926a0173487ad1c08c1108508ba90de67e49
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-01-17 11:59:52 +0100

    Added final V1

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,15 @@
-/*
-- Soll die Linked List auch ein `tail` haben, oder nur einen `head`?
-- Wie iteriert man generell über Objekte?
-
-TODOS:
-- access modifier hinzufügen
-- static hinzufügen
-- Methoden zu LinkedList hinzufügen:
-    - [x] add
-    - [ ] remove
-    - [ ] size
-    - [ ] printList
-    - [ ] get
- */
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.addNode(2);
-        list.addNode(3);
-        System.out.println(list.head.value);
-        System.out.println(list.head.next.value);
-
-
-        // Wie iteriere ich durch die LinkedList? Wie logge ich sie in der Konsole?
-            // Iterator?
-
-
-
-
-
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.printList();
+        System.out.println("list size: " + list.getListSize());
+        list.remove(3);
+        list.printList();
+        System.out.println("list size: " + list.getListSize());
+        System.out.println("node value at index 0: " + list.get(0));
+        System.out.println("node value at index 1: " + list.get(1));
     }
 }
