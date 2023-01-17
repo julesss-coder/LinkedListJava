fe5740ba0f248162b6c51de87d8b6e17d400d982
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-01-17 15:28:16 +0100

    Added exception handling

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,14 @@ public class Main {
         list.remove(3);
         list.printList();
         System.out.println("list size: " + list.getListSize());
-        System.out.println("node value at index 0: " + list.get(0));
+        
+        try {
+            System.out.println("node value at index 0: " + list.get(4));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getLocalizedMessage());
+            System.out.println("Enter a position between 0 and " + (list.getListSize() - 1));
+        }
+        
         System.out.println("node value at index 1: " + list.get(1));
     }
 }
