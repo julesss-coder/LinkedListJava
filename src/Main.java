1eb6705d813da0b97c63c874107379637ea0f7d7
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-01-18 10:32:40 +0100

    Added generics

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
         list.add(2);
         list.add(3);
@@ -9,14 +9,12 @@ public class Main {
         list.remove(3);
         list.printList();
         System.out.println("list size: " + list.getListSize());
-        
         try {
             System.out.println("node value at index 0: " + list.get(4));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getLocalizedMessage());
             System.out.println("Enter a position between 0 and " + (list.getListSize() - 1));
         }
-        
         System.out.println("node value at index 1: " + list.get(1));
     }
 }
